d450fa9683427459bdbfd3cec31f9ccc51c0d6d1
nguyentienanhvuongchuse <nguyenvuong.09062000@gmal.com>
2021-01-27 21:11:49 +0700

    API Django

---
+++ shop/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+	path("product/",ProductView.as_view(),name="product"),
+	path("product/<int:id>/",ProductView.as_view(),name="product-detail"),
+
+]
